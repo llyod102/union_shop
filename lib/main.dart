a07d1c05851acfbaee33f350f9feb268c828d1cf
llyod102 <joshuaibeji332@gmail.com>
2025-12-03 18:22:08 +0000

    Made clicking person icon go to account page

--- lib/main.dart
+++ lib/main.dart
@@ -541,18 +541,20 @@ class _HomeScreenState extends State<HomeScreen> {
                                     });
                                   }),
                               IconButton(
-                                icon: const Icon(
-                                  Icons.person_outline,
-                                  size: 18,
-                                  color: Colors.grey,
-                                ),
-                                padding: const EdgeInsets.all(8),
-                                constraints: const BoxConstraints(
-                                  minWidth: 32,
-                                  minHeight: 32,
-                                ),
-                                onPressed: placeholderCallbackForButtons,
-                              ),
+                                  icon: const Icon(
+                                    Icons.person_outline,
+                                    size: 18,
+                                    color: Colors.grey,
+                                  ),
+                                  padding: const EdgeInsets.all(8),
+                                  constraints: const BoxConstraints(
+                                    minWidth: 32,
+                                    minHeight: 32,
+                                  ),
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                        context, '/authentication');
+                                  }),
                               IconButton(
                                 icon: const Icon(
                                   Icons.shopping_bag_outlined,
